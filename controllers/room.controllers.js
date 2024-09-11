a0261085d2248cc0cdc0cf3402c3afc92457eca2
Ahmed Aladdin <aamieeu@gmail.com>
2024-09-11 08:41:31 +0300

    Design 'Join Room' logic

--- controllers/room.controllers.js
+++ controllers/room.controllers.js
@@ -23,3 +23,42 @@ export const createRoom = async (io, client, { nickname }) => {
     console.error(error);
   }
 };
+
+export const joinRoom = async (io, client, { nickname, roomID }) => {
+  if (!roomID.match(/^[0-9a-fA-F]{24}$/)) {
+    client.emit("join-error", "Please enter a valid room ID");
+    return;
+  }
+
+  const room = await Room.findById(roomID);
+  if (!room) {
+    client.emit("join-error", "Room was not found");
+    return;
+  }
+
+  if (room.isJoinable) {
+    const player = {
+      nickname,
+      socketID: client.id,
+      character: "O",
+    };
+
+    room.players.push(player);
+    room.isJoinable = false;
+    const roomDoc = await room.save();
+
+    //
+    console.log("player tries joining to a room");
+    console.log(player);
+
+    client.join(roomID);
+    io.to(roomID).emit("join-room-success", roomDoc);
+    roomPreChars.set(roomID, "O");
+
+    //
+    console.log(`players of the room\n${getSocketIdsInRoom(roomID, io)}`);
+  } else {
+    client.emit("join-error", "Room is full, try creating a new room");
+    return;
+  }
+};
